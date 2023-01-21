f3f5137f157519b96c7494aacf5610c9508bb26a
Filip Boye-Kofi <Spuxy@seznam.cz>
2023-01-21 22:41:29 +0100

    9.1 Auth package

---
+++ business/web/mid/auth.go
@@ -0,0 +1,48 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/Spuxy/service/business/sys/auth"
+	"github.com/Spuxy/service/foundation/web"
+)
+
+// Authenticate validates a JWT from the `Authorization` header.
+func Authenticate(a *auth.Auth) web.Middleware {
+
+	// This is the actual middleware function to be executed.
+	m := func(handler web.Handler) web.Handler {
+
+		// Create the handler that will be attached in the middleware chain.
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			// Expecting: bearer <token>
+			authStr := r.Header.Get("authorization")
+
+			// Parse the authorization header.
+			parts := strings.Split(authStr, " ")
+			if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+				// _ := errors.New("expected authorization header format: bearer <token>")
+				// return v1Web.NewRequestError(err, http.StatusUnauthorized)
+			}
+
+			// Validate the token is signed by us.
+			// claims  := a.ValidateToken(parts[1])
+			// if err != nil {
+			// 	return v1Web.NewRequestError(err, http.StatusUnauthorized)
+			// }
+
+			// Add claims to the context, so they can be retrieved later.
+			// ctx = auth.SetClaims(ctx, claims)
+
+			// Call the next handler.
+			return handler(ctx, w, r)
+		}
+
+		return h
+	}
+
+	return m
+}
