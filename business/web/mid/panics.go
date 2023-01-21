74cc33c8c5677320bed62e82d1f09547fa03eac1
Filip Boye-Kofi <Spuxy@seznam.cz>
2023-01-21 11:24:21 +0100

    7.4 Panic Handling

---
+++ business/web/mid/panics.go
@@ -0,0 +1,24 @@
+package mid
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Spuxy/service/foundation/web"
+)
+
+func Panics() web.Middleware {
+	m := func(handler web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) (err error) {
+			defer func() {
+				if rec := recover(); rec != nil {
+					err = fmt.Errorf("PANIC [%v]", rec)
+				}
+			}()
+			return handler(ctx, w, r)
+		}
+		return h
+	}
+	return m
+}
